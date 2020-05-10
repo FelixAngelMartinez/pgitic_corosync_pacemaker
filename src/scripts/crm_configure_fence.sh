3f8bba7f62c4124858cb0b20f30ac78c06cb7736
Miguel <miguelcal97@gmail.com>
2020-05-10 17:39:34 +0200

    Final code update

--- src/scripts/crm_configure_fence.sh
+++
@@ -1,6 +0,0 @@
-sudo crm -F configure primitive fence_nodo1 stonith:fence_virsh params ipaddr=192.168.128.101 port=22 action=off login=vagrant passwd=vagrant op monitor interval=60s
-sudo crm -F configure primitive fence_nodo2 stonith:fence_virsh params ipaddr=192.168.128.101 port=22 action=off login=vagrant passwd=vagrant delay=15 op monitor interval=60s
-sudo crm -F configure primitive fence_nodo3 stonith:fence_virsh params ipaddr=192.168.128.101 port=22 action=off login=vagrant passwd=vagrant delay=15 op monitor interval=60s
-sudo crm -F configure location l_fence_nodo1 fence_nodo1 -inf: nodo1
-sudo crm -F configure location l_fence_nodo2 fence_nodo2 -inf: nodo2
-sudo crm -F configure location l_fence_nodo3 fence_nodo3 -inf: nodo3
